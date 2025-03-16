bf3e24466c3410aa9188e64674619ef36f146df5
crumblingstatue <radiantstatue@gmail.com>
2025-03-16 08:51:24 +0100

    Use cmd_lib for fun

--- build-wasm.rs
+++ build-wasm.rs
@@ -1,33 +1,18 @@
 #!/usr/bin/env -S cargo +nightly -Zscript
-#![feature(anonymous_pipe)]
-
-use std::process::Command;
+---cargo
+[dependencies]
+cmd_lib = "1.9.5"
+---
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("cargo")
-        .args([
-            "build",
-            "--target=wasm32-unknown-unknown",
-            "--release",
-            "-Zbuild-std=std,panic_abort",
-            "-Zbuild-std-features=panic_immediate_abort",
-        ])
-        .status()?;
-    let (r, w) = std::io::pipe()?;
-    Command::new("cargo").arg("metadata").stdout(w).spawn()?;
-    let out = Command::new("jq")
-        .arg(".target_directory")
-        .stdin(r)
-        .output()?
-        .stdout;
-    let target_dir = String::from_utf8(out)?;
-    let target_dir = target_dir
-        .trim()
-        .trim_start_matches('"')
-        .trim_end_matches('\"');
+	cmd_lib::run_cmd! {
+		cargo build
+			--target=wasm32-unknown-unknown
+			--release
+			-Zbuild-std=std,panic_abort -Zbuild-std-features=panic_immediate_abort
+	}?;
+	let target_dir = cmd_lib::run_fun!(cargo metadata | jq -r .target_directory)?;
     let path = format!("{target_dir}/wasm32-unknown-unknown/release/graphical-solve.wasm");
-    Command::new("wasm-opt")
-        .args(["-Os", "--strip-debug", &path, "-o", "graphical-solve.wasm"])
-        .status()?;
+    cmd_lib::run_cmd!(wasm-opt -Os --strip-debug $path -o graphical-solve.wasm)?;
     Ok(())
 }
